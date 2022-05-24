b32bccfa04c3be41f72baba594a4b51292ae1ad2
unknown <alena.jonjic.20@size.ba.com>
2022-05-24 16:34:26 +0200

    book cards, authors dropdown (get)

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
 const BASE_URL = "https://ptf-web-dizajn-2022.azurewebsites.net";
+
+let books = [];
+let authors = [];
+
+fetch(`${BASE_URL}/books`)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    books = data;
+    renderBooks(data);
+  });
+fetch(`${BASE_URL}/authors`)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    authors = data;
+    renderAuthors(data);
+  });
+const renderBooks = (books) => {
+  const booksRow = document.getElementById("books-row");
+
+  let resultBooksHtml = "";
+
+  books.forEach((book) => {
+    resultBooksHtml += `
+        <div class="card mx-2 my-4" style="width: 18rem;">
+            <img src="${book.image}" class="card-img-top my-1" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${book.name}</h5>
+                <p class="card-text">${book.author.name} </p>
+                <p class="card-text">Genre: ${book.genre}</p>
+                <button id="add-favorites-button" type="button" class="btn btn-primary" onclick="getElementById('${book.id}')">
+                <i class="bi bi-bookmark-heart"></i>
+                </button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal" onclick="editBook('${book.id}')">Uredi</button>
+                <button type="button" onclick="deleteBook('${book.id}')" class="btn btn-danger">Izbriši</button>
+            </div>
+        </div>
+        `;
+  });
+  booksRow.innerHTML = resultBooksHtml;
+};
+let dropDownListUpdate = document.getElementById("dropdownAuthorsSec");
+let dropDownLists = document.getElementById("dropdownAuthors");
+const renderAuthors = (authors) => {
+  //popunjavanje drop down liste (za update Modal i add Book Modal)
+  let dropDownListHtml = " ";
+  authors.forEach((author) => {
+    dropDownListHtml += `
+     <option value="${author.id}">${author.name}</option>`;
+  });
+  dropDownLists.innerHTML = dropDownListHtml;
+  dropDownListUpdate.innerHTML = dropDownListHtml;
+};
